e9d1c7d99d8d9ff0d97239a54c844676f6452afc
Emilio Barbosa <emilio.projetos@gmail.com>
2024-10-16 10:18:27 -0300

    funções de ecolha de jogo

--- script.js
+++ script.js
@@ -1,3 +1,18 @@
 const playHuman = (humanChoice) => {
-    console.log(humanChoice)
+    
+    playTheGame(humanChoice, playMachine())
+
+}
+
+const playMachine = () => {
+    const choices = ['pedra', 'papel', 'tesoura']
+    const randomNumber = Math.floor(Math.random() * 3)
+
+
+    return choices[randomNumber]
+}
+
+const playTheGame = (human, machine) => {
+    
+    console.log("Humano " + human + "Maquina " + machine)
 }
